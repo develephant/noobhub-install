de97854e41d7b8c06a029b38362a1d8195c06eb6
develephant <code@develephant.com>
2018-01-30 15:21:39 -0600

    init

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+WD=`pwd`
+INSTALLDIR=/usr/local/noobhub
+
+echo '=> Installing NoobHub...'
+
+wait 2
+
+apt-get update
+
+npm install forever -g
+
+mkdir $INSTALLDIR
+
+cd $INSTALLDIR
+
+git clone https://github.com/Overtorment/NoobHub.git
+
+ufw allow 1337/tcp
+
+cp ./bin/noobhub /usr/local/bin/noobhub
+
+chmod /usr/local/bin a+x noobhub
+
+noobhub start
+
+echo ''
+echo '*******************************************************************'
+echo '=>NoobHub is now running on port 1337!'
+echo '=>Installed in /usr/local/noobhub'
+echo ''
+echo 'Type: `noobhub help` for CLI options'
+echo ''
+echo '=>Installer brought to you by develephant.com'
+echo '*******************************************************************'
+echo ''
+
+exit 0
